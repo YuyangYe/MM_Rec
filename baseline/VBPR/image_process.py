d2c3a67e978fa9ad0bdf329ea4e6de2fad57dfec
YuyangYe <yeyuyang95@gmail.com>
2024-03-17 01:30:56 -0400

    update

--- baseline/VBPR/image_process.py
+++ baseline/VBPR/image_process.py
@@ -5,10 +5,11 @@ from torchvision.models.resnet import ResNet50_Weights
 from PIL import Image
 import os
 import tqdm
+import re
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-def extract_features(image_paths):
+def extract_features(image_paths, fwrite):
     preprocess = transforms.Compose([
         transforms.Resize(256),
         transforms.CenterCrop(224),
@@ -27,7 +28,7 @@ def extract_features(image_paths):
     for i in tqdm.trange(len(os.listdir(image_paths))):
         image_path = os.listdir(image_paths)[i]
         image_path = os.path.join(image_paths, image_path)
-        print(image_path)
+        #print(image_path)
         image = Image.open(image_path).convert('RGB')
         image = preprocess(image).unsqueeze(0).to(device)
 
@@ -35,14 +36,15 @@ def extract_features(image_paths):
             features = model(image)
 
         features = features.squeeze().cpu().numpy()
-        features_list.append(features)
+
+        img_idx = re.search(r'/(\d+)\.jpg$', image_path).group(1)
+        print(img_idx)
+        line = [img_idx] + list(map(str, features))
+        fwrite.write(','.join(line) + '\n')
 
     return features_list
 
 f = open('image_features.csv', 'w')
-features = extract_features('../../data/MicroLens-50k/MicroLens-50k_covers')
-for i in tqdm.trange(len(features)):
-    line = [str(i + 1)] + list(map(str, features[i]))
-    f.write(','.join(line) + '\n')
+features = extract_features('../../data/MicroLens-50k/MicroLens-50k_covers', f)
 f.close()
 print("Image features saved to image_features.csv")
--- baseline/VBPR/vbpr.py
+++ baseline/VBPR/vbpr.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn
+import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
 import pandas as pd
 import numpy as np
@@ -7,14 +8,16 @@ from sklearn.model_selection import train_test_split
 from utils import ndcg_at_k, f1_at_k
 import tqdm
 import csv
+import os
+
+os.environ["CUDA_VISIBLE_DEVICES"]="4,5,6,7"
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 class InteractionDataset(Dataset):
-    def __init__(self, data_frame, item_features):
+    def __init__(self, data_frame):
         self.data = data_frame
-        self.item_features = item_features
         self.user_item_pairs = [tuple(x) for x in self.data.to_numpy()]
         self.all_users = self.data['user'].unique()
         self.all_items = self.data['item'].unique()
@@ -24,39 +27,50 @@ class InteractionDataset(Dataset):
 
     def __getitem__(self, idx):
         user, item = self.user_item_pairs[idx]
-        item_int = int(item)
-        print(item_int)
-        item_features = torch.tensor(self.item_features.loc[item_int].values, dtype=torch.float32).to(device)
         neg_item = np.random.choice(self.all_items)
         while neg_item == item:
             neg_item = np.random.choice(self.all_items)
-        neg_item_int = int(neg_item)
-        neg_item_features = torch.tensor(self.item_features.loc[neg_item_int].values, dtype=torch.float32).to(device)
-        return user, item, item_features, neg_item, neg_item_features
+        return user, item, neg_item
 
 
 class VBPR(nn.Module):
-    def __init__(self, num_users, num_items, factors=64):
+    def __init__(self, num_users, num_items, item_feature, dim_feature, factors=128):
         super(VBPR, self).__init__()
+        self.item_features = item_feature
         self.user_embedding = nn.Embedding(num_users, factors).to(device)
         self.item_embedding = nn.Embedding(num_items, factors).to(device)
-        self.visual_embedding = nn.Embedding(num_items, factors).to(device)
-        # self.user_bias = nn.Embedding(num_users, 1).to(device)
-        # self.item_bias = nn.Embedding(num_items, 1).to(device)
-        self.visual_bias = nn.Embedding(num_items, 1).to(device)
+        self.visual_embedding = nn.Linear(dim_feature, factors).to(device)
+        self.visual_bias = nn.Linear(dim_feature, 1).to(device)
+        self.attention_layer = nn.Linear(factors, 1).to(device)
         nn.init.normal_(self.user_embedding.weight, std=0.01)
         nn.init.normal_(self.item_embedding.weight, std=0.01)
         nn.init.normal_(self.visual_embedding.weight, std=0.01)
-        # nn.init.zeros_(self.user_bias.weight)
-        # nn.init.zeros_(self.item_bias.weight)
         nn.init.zeros_(self.visual_bias.weight)
 
-    def forward(self, user, item_i, feature_i, item_j, feature_j):
+    def forward(self, user, item_i, item_j):
         user_emb = self.user_embedding(user)
         item_i_emb = self.item_embedding(item_i)
         item_j_emb = self.item_embedding(item_j)
+        item_i = item_i.cpu().numpy().tolist()
+        item_j = item_j.cpu().numpy().tolist()
+        feature_i = torch.tensor(self.item_features.loc[item_i].values, dtype=torch.float32).to(device)
+        feature_j = torch.tensor(self.item_features.loc[item_j].values, dtype=torch.float32).to(device)
         visual_i_emb = self.visual_embedding(feature_i)
         visual_j_emb = self.visual_embedding(feature_j)
+
+        attention_item_i = torch.tanh(self.attention_layer(item_i_emb).squeeze())
+        attention_item_j = torch.tanh(self.attention_layer(item_j_emb).squeeze())
+        attention_visual_i = torch.tanh(self.attention_layer(visual_i_emb).squeeze())
+        attention_visual_j = torch.tanh(self.attention_layer(visual_j_emb).squeeze())
+
+        attention_weights_i = F.softmax(torch.cat([attention_item_i, attention_visual_i], dim=1), dim=1)
+        attention_weights_j = F.softmax(torch.cat([attention_item_j, attention_visual_j], dim=1), dim=1)
+
+        item_i_emb = attention_weights_i[:, 0].unsqueeze(1) * item_i_emb
+        visual_i_emb = attention_weights_i[:, 1].unsqueeze(1) * visual_i_emb
+        item_j_emb = attention_weights_j[:, 0].unsqueeze(1) * item_j_emb
+        visual_j_emb = attention_weights_j[:, 1].unsqueeze(1) * visual_j_emb
+
         prediction_i = (user_emb * item_i_emb).sum(dim=1) + (user_emb * visual_i_emb).sum(dim=1) + self.visual_bias(
             feature_i).squeeze()
         prediction_j = (user_emb * item_j_emb).sum(dim=1) + (user_emb * visual_j_emb).sum(dim=1) + self.visual_bias(
@@ -66,21 +80,23 @@ class VBPR(nn.Module):
     def predict(self, user, item):
         user_emb = self.user_embedding(user)
         item_emb = self.item_embedding(item)
-        feature_emb = self.visual_embedding(item)
-        feature_bias = self.visual_bias(item).squeeze()
+        item = item.cpu().numpy().tolist()
+        feature = torch.tensor(self.item_features.loc[item].values, dtype=torch.float32).to(device)
+        feature_emb = self.visual_embedding(feature)
+        feature_bias = self.visual_bias(feature).squeeze()
 
         score = (user_emb * item_emb).sum(dim=1) + (user_emb * feature_emb).sum(dim=1) + feature_bias
         return score
 
 
-def train(model, data_loader, optimizer, epochs=10):
+def train(model, data_loader, optimizer, epochs=1):
     model.train()
     criterion = nn.LogSigmoid()
-    for epoch in range(epochs):
-        for user, item_i, feature_i, item_j, feature_j in data_loader:
-            user, item_i, feature_i, item_j, feature_j = user.to(device), item_i.to(device), feature_i.to(device), item_j.to(device), feature_j.to(device)
+    for epoch in tqdm.trange(epochs):
+        for user, item_i, item_j in data_loader:
+            user, item_i, item_j= user.to(device), item_i.to(device), item_j.to(device)
             optimizer.zero_grad()
-            prediction = model(user, item_i, feature_i, item_j, feature_j)
+            prediction = model(user, item_i, item_j)
             loss = -criterion(prediction).mean()
             loss.backward()
             optimizer.step()
@@ -92,13 +108,12 @@ def generate_recommendations(model, user_ids, all_item_ids, k):
     recommendations = {}
 
     with torch.no_grad():
-        for i in tqdm(range(len(user_ids))):
+        for i in tqdm.trange(len(user_ids)):
             user_id = user_ids[i]
             user_id_tensor = torch.tensor([user_id] * len(all_item_ids), dtype=torch.long, device=device)
             item_ids_tensor = torch.tensor(all_item_ids, dtype=torch.long, device=device)
-            feature_ids_tensor = torch.tensor(all_item_ids, dtype=torch.long, device=device)
 
-            scores = model.predict(user_id_tensor, item_ids_tensor, feature_ids_tensor).cpu().numpy()
+            scores = model.module.predict(user_id_tensor, item_ids_tensor).cpu().numpy()
 
             # select top k items
             recommended_item_ids = np.argsort(scores)[-k:][::-1]
@@ -136,7 +151,6 @@ def evaluate_model(model, test_data, all_items, k):
 num_visual_features = len(next(csv.reader(open('image_features.csv', 'r'), delimiter=','))) - 1
 visual_emb_df = pd.read_csv('image_features.csv', header=None,
                             names=['original_id'] + [f'feature_{i}' for i in range(num_visual_features)])
-print(visual_emb_df.loc[visual_emb_df['original_id'] == 19274])
 
 item_mapping = pd.read_csv('../../data/process/item_mapping.csv')
 updated_visual_features_df = pd.merge(visual_emb_df, item_mapping, on='original_id')
@@ -145,13 +159,12 @@ final_columns = ['new_id'] + [f'feature_{i}' for i in range(num_visual_features)
 final_visual_features_df = updated_visual_features_df[final_columns]
 final_visual_features_df.rename(columns={'new_id': 'item'}, inplace=True)
 final_visual_features_df.set_index('item', inplace=True)
-print(final_visual_features_df.loc[14308].values)
 
 dataset = pd.read_csv('../../data/MicroLens-50k/MicroLens-50k_pairs_processed.csv', usecols=["user", "item"])
 train_data, test_data = train_test_split(dataset, test_size=0.2, shuffle=True)
 
-train_dataset = InteractionDataset(train_data, final_visual_features_df)
-# test_dataset = InteractionDataset(test_data,final_visual_features_df)
+train_dataset = InteractionDataset(train_data)
+# test_dataset = InteractionDataset(test_data)
 
 train_loader = DataLoader(train_dataset, batch_size=128, shuffle=True)
 # test_loader = DataLoader(test_dataset, batch_size=128, shuffle=False)
@@ -160,7 +173,10 @@ nu = len(np.unique(dataset['user']))
 ni = len(np.unique(dataset['item']))
 print(f'Number of users: {nu}, Number of items: {ni}')
 
-model = VBPR(num_users=nu, num_items=ni).to(device)
+model = VBPR(num_users=nu, num_items=ni, item_feature=final_visual_features_df, dim_feature=num_visual_features, factors=128)
+if torch.cuda.device_count() > 1:
+    model = nn.DataParallel(model)
+model.to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
 train(model, train_loader, optimizer)
 evaluate_model(model, test_data, ni, k=10)
